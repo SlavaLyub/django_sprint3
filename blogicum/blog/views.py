f82734f68a0c5d74abc3eed246e5db78a3559a74
Вячеслав Любченко <vyacheslav.liubchenko@yandex.ru>
2023-11-10 20:47:44 +0300

    оптимизировал запросы

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,34 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-from blog.models import Post, Category
+from .models import Post, Category
 
 
-def index(request):
-    template = 'blog/index.html'
-    posts_index_list = Post.objects.select_related(
+def get_posts():
+    return Post.objects.select_related(
+        'location',
         'category',
         'author',
-        'location',
     ).filter(
         is_published=True,
         pub_date__lte=timezone.now(),
         category__is_published=True,
-    )[:5]
+    )
+
+
+def index(request):
+    template = 'blog/index.html'
+    posts_index_list = get_posts()[:5]
     context = {'post_list': posts_index_list}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-        Post.objects.select_related(
-            'location',
-            'category'
-        ).filter(
-            is_published=True,
-            pub_date__lte=timezone.now(),
-            category__is_published=True,
-        ),
+        get_posts(),
         pk=post_id,
     )
     context = {'post': post}
@@ -39,18 +36,11 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     category = get_object_or_404(
         Category.objects.filter(
-            slug=category_slug,
-            is_published=True,
-        )
+            is_published=True
+        ),
+        slug=category_slug,
     )
-
-    post_list = Post.objects.select_related(
-        'location',
-        'category',
-        'author'
-    ).filter(
-        is_published__exact=True,
-        pub_date__lte=timezone.now(),
+    post_list = get_posts().filter(
         category__slug=category_slug,
     )
 
